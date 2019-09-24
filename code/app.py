4200b4c1a19602a10193573632cfaf7d9a0fdf9a
Jeff <theta_158@yahoo.com>
2019-09-24 12:14:18 -0400

    created create_app function

--- code/app.py
+++ code/app.py
@@ -4,28 +4,31 @@ from flask import Flask, request, jsonify
 
 model = joblib.load('nba_model')
 
-app = Flask(__name__)
 
+def create_app():
 
-@app.route('/api', methods=['POST'])
-def predict():
-    # get the data
-    data = request.get_json(force=True)
+    app = Flask(__name__)
 
-    predict_request = [data['all_nba'], data['all_star'], data['draft_yr'], data['pk'],
-                       data['fg_percentage'], data['tp_percentage'], data['ft_percentage'],
-                       data['minutes_per_game'], data['points_per_game'], data['trb_per_game'],
-                       data['assists_per_game'], data['ws_per_game'], data['bpm'], data['vorp'],
-                       data['attend_college']]
-    
-    array_pred_req = np.array(predict_request).reshape(1, -1)
+    @app.route('/api', methods=['POST'])
+    def predict():
+        # get the data
+        data = request.get_json(force=True)
 
-    # predictions
-    y_pred = model.predict(array_pred_req)
+        predict_request = [data['all_nba'], data['all_star'], data['draft_yr'], data['pk'],
+                           data['fg_percentage'], data['tp_percentage'], data['ft_percentage'],
+                           data['minutes_per_game'], data['points_per_game'], data['trb_per_game'],
+                           data['assists_per_game'], data['ws_per_game'], data['bpm'], data['vorp'],
+                           data['attend_college']]
 
-    # give it back
-    output = {"y_pred": int(y_pred[0])}
-    return jsonify(results=output)
+        array_pred_req = np.array(predict_request).reshape(1, -1)
+
+        # predictions
+        y_pred = model.predict(array_pred_req)
+
+        # give it back
+        output = {"y_pred": int(y_pred[0])}
+        return jsonify(results=output)
+    return app
 
 
 if __name__ == '__main__':
